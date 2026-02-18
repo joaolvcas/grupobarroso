5af8eb48574ccca4889422d403acafdcbad03b42
joaolvcas <joacamilo.br@gmail.com>
2026-02-18 10:29:11 -0300

    Corrige Layout

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const GlobalContext = createContext();
 
 export function GlobalProvider({ children }) {
-  const [brand, setBrand] = useState("");
+  const [brand, setBrand] = useState("placas");
 
   return (
     <GlobalContext.Provider value={{ brand, setBrand }}>
